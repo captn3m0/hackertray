cff96cb98be216e704c65874a1fa20703dc19a7e
Abhay Rana <capt.n3m0@gmail.com>
2013-11-28 17:37:32 +0530

    Change package URL and update description

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
 
 setup(name='hackertray',
-      version='1.1',
+      version='1.2',
       description='Hacker News app that sits in your System Tray',
-      url='http://github.com/captn3m0/hackertray',
+      long_description='HackerTray is a simple Hacker News Linux application that lets you view top HN stories in your System Tray. It relies on appindicator, so it is not guaranteed to work on all systems. It also provides a StatusIcon fallback but it has not been tested.',
+      keywords='hacker news hn tray system tray icon hackertray',
+      url='http://captnemo.in/hackertray',
       author='Abhay Rana',
       author_email='me@captnemo.in',
       license='MIT',
