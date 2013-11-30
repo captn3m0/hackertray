2a6c475d31971e2ad193272f56dc5eb31e4f25e7
Abhay Rana <capt.n3m0@gmail.com>
2013-11-30 11:34:06 +0530

    Adds hacker-tray icon to the package. Fixes #11 Thanks to @Gonzih for providing a transparent, better looking icon.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='hackertray',
-      version='1.6',
+      version='1.7',
       description='Hacker News app that sits in your System Tray',
       long_description='HackerTray is a simple Hacker News Linux application that lets you view top HN stories in your System Tray. It relies on appindicator, so it is not guaranteed to work on all systems. It also provides a Gtk StatusIcon fallback in case AppIndicator is not available.',
       keywords='hacker news hn tray system tray icon hackertray',
@@ -10,8 +10,7 @@ setup(name='hackertray',
       author_email='me@captnemo.in',
       license='MIT',
       packages=['hackertray'],
-      package_data={'hackertray': ['../images/hacker-tray.png']},
-      include_package_data=True,
+      data_files=[('images', ['images/hacker-tray.png'])],
       install_requires=[
           'requests',
       ],
