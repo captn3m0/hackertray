ca0bd63dda9746c542c57e4262a174125d2c09ec
Abhay Rana <capt.n3m0@gmail.com>
2013-11-30 17:40:49 +0530

    Use only the main API server, as per @cheeaun's request

--- hackertray/hackernews.py
+++ hackertray/hackernews.py
@@ -2,13 +2,7 @@ import random
 import requests
 
 urls = [
-	'http://node-hnapi-eu.herokuapp.com/', # Heroku (EU)
-	'http://node-hnapi.azurewebsites.net/', # Windows Azure (North EU)
-	'http://node-hnapi-asia.azurewebsites.net/', # Windows Azure (East Asia)
-	'http://node-hnapi-eus.azurewebsites.net/', # Windows Azure (East US)
-	'http://node-hnapi-weu.azurewebsites.net/', # Windows Azure (West EU)
-	'http://node-hnapi-wus.azurewebsites.net/', # Windows Azure (West US)
-	'http://node-hnapi-ncus.azurewebsites.net/' # Windows Azure (North Central US)
+	'https://node-hnapi.herokuapp.com/'
 ];
 
 class HackerNews:
