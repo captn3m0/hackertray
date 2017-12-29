b1d1e3467c6c7f3cd9a50b3169923c5477d98074
kh99 <kailash.hiwale@powerupcloud.com>
2017-12-29 13:15:04 +0530

    Update: python3 porting and PEP8 style coding in analytics.py appindicator_replacement.py chrome.py firefox.py hackernews.py version.py

--- hackertray/version.py
+++ hackertray/version.py
@@ -1,8 +1,10 @@
 import requests
 import pkg_resources
 
+
 class Version:
     PYPI_URL = "https://pypi.python.org/pypi/hackertray/json"
+
     @staticmethod
     def latest():
         res = requests.get(Version.PYPI_URL).json()
@@ -14,14 +16,16 @@ class Version:
 
     @staticmethod
     def new_available():
-        latest  = Version.latest()
+        latest = Version.latest()
         current = Version.current()
         try:
             if pkg_resources.parse_version(latest) > pkg_resources.parse_version(current):
-                print "[+] New version " + latest + " is available"
+                print("[+] New version " + latest + " is available")
                 return True
+
             else:
                 return False
+
         except requests.exceptions.RequestException as e:
-            print "[+] There was an error in trying to fetch updates"
+            print("[+] There was an error in trying to fetch updates")
             return False
